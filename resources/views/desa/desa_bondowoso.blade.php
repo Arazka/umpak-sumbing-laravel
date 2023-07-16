e42ca6b1551d520b1829c9f3deb7b51510077ad0
Zarthan <46505448+Zarthan66@users.noreply.github.com>
2023-07-17 01:51:41 +0700

    Pariwisata adding progress

---
+++ resources/views/desa/desa_bondowoso.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('main')
+<div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">Blog Title</h1>
+        <hr>
+        <article>
+          <h2>Blog Post Title</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa eros. Nullam fringilla tortor vel
+            mauris pulvinar, et iaculis elit rutrum. Donec eget varius sem. Phasellus sagittis felis at volutpat
+            tristique. Nulla consectetur metus in nisi scelerisque, sit amet ultricies dolor efficitur. In semper
+            metus id ligula elementum fringilla. Vivamus pellentesque enim non mauris feugiat pharetra. Nulla id
+            sodales orci, ut bibendum ex.</p>
+          <img src="image.jpg" class="img-fluid mt-3" alt="Blog Image">
+          <p>Aliquam euismod quam vitae aliquet semper. Ut sed nibh aliquam, efficitur sapien ut, lacinia quam. In
+            sagittis elementum justo, in facilisis arcu aliquet a. Vestibulum vestibulum purus nulla, sed mollis
+            dui viverra eu. Proin et rhoncus est, in consequat lorem. Morbi sodales tellus eget lectus bibendum
+            condimentum. Integer vitae turpis nec arcu finibus mattis in ac lacus. Phasellus nec bibendum eros.</p>
+          <p>Etiam facilisis viverra tellus, sed feugiat nibh tristique ac. Pellentesque eu diam rutrum, vehicula
+            elit eu, gravida enim. Aliquam vitae ante lectus. Fusce a turpis a est egestas convallis at eget quam.
+            Sed a sollicitudin justo. Morbi vel cursus enim, eu convallis odio. In hac habitasse platea dictumst.
+            Nullam ut congue tellus. Ut dapibus felis a dui iaculis vestibulum. Fusce tristique felis sit amet
+            tincidunt tincidunt. Nullam egestas bibendum nunc, sit amet tristique ipsum gravida eu.</p>
+        </article>
+      </div>
+    </div>
+  </div>
+
+@endsection
--- resources/views/pariwisata/pariwisata.blade.php
+++ resources/views/pariwisata/pariwisata.blade.php
@@ -1,12 +1,130 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>pariwisata</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('main')
+<div class="container container-header">
+    <nav>
+        <div class="nav nav-tabs d-flex align-items-center justify-content-center" id="nav-tab" role="tablist">
+            <button class="nav-link active" id="desa-bandongan-tab" data-bs-toggle="tab" data-bs-target="#desa-bandongan" type="button" role="tab" aria-controls="desa-bandongan" aria-selected="true">Desa Bandongan</button>
+            <button class="nav-link" id="desa-gandusari-tab" data-bs-toggle="tab" data-bs-target="#desa-gandusari" type="button" role="tab" aria-controls="desa-gandusari" aria-selected="false">Desa Gandusari</button>
+            <button class="nav-link" id="desa-kalegen-tab" data-bs-toggle="tab" data-bs-target="#desa-kalegen" type="button" role="tab" aria-controls="desa-kalegen" aria-selected="false">Desa Kalegen</button>
+            <button class="nav-link" id="desa-ngepanrejo-tab" data-bs-toggle="tab" data-bs-target="#desa-ngepanrejo" type="button" role="tab" aria-controls="desa-ngepanrejo" aria-selected="false">Desa Ngepanrejo</button>
+            <button class="nav-link" id="desa-rejosari-tab" data-bs-toggle="tab" data-bs-target="#desa-rejosari" type="button" role="tab" aria-controls="desa-rejosari" aria-selected="false">Desa Rejosari</button>
+            <button class="nav-link" id="desa-sidorejo-tab" data-bs-toggle="tab" data-bs-target="#desa-sidorejo" type="button" role="tab" aria-controls="desa-sidorejo" aria-selected="false">Desa Sidorejo</button>
+            <button class="nav-link" id="desa-trasan-tab" data-bs-toggle="tab" data-bs-target="#desa-trasan" type="button" role="tab" aria-controls="desa-trasan" aria-selected="false">Desa Trasan</button>
+        </div>
+    </nav>
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="desa-bandongan" role="tabpanel" aria-labelledby="desa-bandongan-tab" tabindex="0">
+            <div class="card projects-img">
+                <a href="your-page-url">
+                    <img src="img/desa_geo_map/desa_bandongan.png" class="card-img-top" alt="projects1" style="height:25rem; object-fit: cover;" />
+                </a>
+                <div class="card-body align-items-center justify-content-center">
+                    <h5 class="card-title text-center">Desa Bandongan</h5>
+                    <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="text-center"> <!-- Add text-center class here -->
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade align-items-center justify-content-center" id="desa-gandusari" role="tabpanel" aria-labelledby="desa-gandusari-tab" tabindex="0">
+            <div class="card projects-img">
+                <a href="your-page-url">
+                    <img src="img/desa_geo_map/desa_gandusari.png" class="card-img-top" alt="projects1" style="height:25rem; object-fit: cover;" />
+                </a>
+                <div class="card-body align-items-center justify-content-center">
+                    <h5 class="card-title text-center">Desa Gandusari</h5>
+                    <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="text-center"> <!-- Add text-center class here -->
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="desa-kalegen" role="tabpanel" aria-labelledby="desa-kalegen-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="desa-ngepanrejo" role="tabpanel" aria-labelledby="desa-ngepanrejo-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="desa-rejosari" role="tabpanel" aria-labelledby="desa-rejosari-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="desa-sidorejo" role="tabpanel" aria-labelledby="desa-sidorejo-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="desa-trasan" role="tabpanel" aria-labelledby="desa-trasan-tab" tabindex="0">...</div>
+    </div>
+</div>
+
+<!-- DESA -->
+<section id="desa">
+    <div class="container mt-5">
+        <div class="row justify-content-left">
+            <div class="col-md-4 mb-4">
+                <div class="card projects-img h-100">
+                    <img src="img/foto desa/desa bandongan.png" class="card-img-top" alt="projects1" style="object-fit: cover; height: 15rem;" />
+                    <div class="card-body">
+                        <h5 class="card-title">Desa Bandongan</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card projects-img h-100">
+                    <img src="img/foto desa/Desa Gandusari.png" class="card-img-top" alt="projects1" style="object-fit: cover; height: 15rem;" />
+                    <div class="card-body">
+                        <h5 class="card-title">Desa Gandusari</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card projects-img h-100">
+                    <img src="img/foto desa/Desa Kalegen.png" class="card-img-top" alt="projects1" style="object-fit: cover; height: 15rem;" />
+                    <div class="card-body">
+                        <h5 class="card-title">Desa Kalegen</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card projects-img h-100">
+                    <img src="img/foto desa/Desa Ngepanrejo.png" class="card-img-top" alt="projects1" style="object-fit: cover; height: 15rem;" />
+                    <div class="card-body">
+                        <h5 class="card-title">Desa Ngepanrejo</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card projects-img h-100">
+                    <img src="img/foto desa/Desa Rejosari.png" class="card-img-top" alt="projects1" style="object-fit: cover; height: 15rem;" />
+                    <div class="card-body">
+                        <h5 class="card-title">Desa Rejosari</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card projects-img h-100">
+                    <img src="img/foto desa/desa sidorejo.png" class="card-img-top" alt="projects1" style="object-fit: cover; height: 15rem;" />
+                    <div class="card-body">
+                        <h5 class="card-title">Desa Sidorejo</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card projects-img h-100">
+                    <img src="img/foto desa/Desa Trasan.png" class="card-img-top" alt="projects1" style="object-fit: cover; height: 15rem;" />
+                    <div class="card-body">
+                        <h5 class="card-title">Desa Trasan</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Baca selengkapnya</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
