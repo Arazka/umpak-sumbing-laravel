d5db994169341bcea692ff5aa3b0f60175bc0825
Zarthan <46505448+Zarthan66@users.noreply.github.com>
2023-07-21 08:14:23 +0700

    entering budesma content

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return view('welcome');});
 Route::get('/umpak-sumbing', function () {return view('welcome');});
 Route::get('/pariwisata', function () {return view('pariwisata/pariwisata');});
+// bkad
 Route::get('/bkad/profil-lembaga', function () {return view('bkad/profilLembaga');});
 Route::get('/bkad/struktur-organisasi', function () {return view('bkad/strukturOrganisasi');});
 Route::get('/bkad/program-kerja', function () {return view('bkad/programKerja');});
 Route::get('/bkad/rencana-pembangunan-kawasan-pedesaan', function () {return view('bkad/rpkp');});
+// budesma
+Route::get('/bumdesma/profil-lembaga', function () {return view('bumdesma/profilLembaga');});
+Route::get('/bumdesma/struktur-organisasi', function () {return view('bumdesma/strukturOrganisasi');});
+Route::get('/bumdesma/program-kerja', function () {return view('bumdesma/programKerja');});
+Route::get('/bumdesma/rencana-pembangunan-kawasan-pedesaan', function () {return view('bumdesma/regulasi');});
 // end route fitur
 
 // detail desa
